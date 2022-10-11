ed95bf0b6ccc8c1f1ffe66cf3664ead8e7dd4bae
chenhewei <1990945244@qq.com>
2022-10-11 21:30:13 +0800

    Nacos -config -discovery

---
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/wei/springcloudAlibaba/OrderNaocsMain83.java
@@ -0,0 +1,14 @@
+package com.wei.springcloudAlibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderNaocsMain83 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderNaocsMain83.class,args);
+    }
+}
